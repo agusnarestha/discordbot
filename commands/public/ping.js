acd4ffde6ef2d4c573c6ae302bde768ca3fa342c
Agus Narestha <63548030+agusnarestha@users.noreply.github.com>
2023-01-13 11:03:46 +0800

    Update command ping

--- commands/public/ping.js
+++ commands/public/ping.js
@@ -1,19 +1,40 @@
 const {
-  ChatInputCommandInteraction,
   SlashCommandBuilder,
+  EmbedBuilder,
+  ChatInputCommandInteraction,
+  PermissionFlagsBits,
+  AttachmentBuilder,
 } = require("discord.js");
 
 module.exports = {
+  developers: true,
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong!"),
+    .setDescription("Bot's Realtime Ping"),
   /**
-   *
    * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
    */
-  execute(interaction) {
-    interaction.reply({
-      content: "Pong!",
+  async execute(interaction, client) {
+    let ping;
+    let color;
+    if (100 > client.ws.ping || 100 === client.ws.ping) {
+      color = "Green";
+    } else if (300 > client.ws.ping && 300 === client.ws.ping) {
+      ping = "<:medium:1063004174750580736>";
+      color = "Orange";
+    } else if (500 > client.ws.ping || 500 === client.ws.ping) {
+      color = "Red";
+    } else {
+      color = "Yellow";
+    }
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(color || "Random")
+          .setTitle("Bot Ping")
+          .setDescription(`**Ping:** \`${client.ws.ping}ms\``),
+      ],
       ephemeral: true,
     });
   },
--- index.js
+++ index.js
@@ -1,90 +1,9 @@
-// const {
-//   Client,
-//   Collection,
-//   Events,
-//   GatewayIntentBits,
-//   REST,
-//   Routes,
-// } = require("discord.js");
-// const { clientId, guildId, token } = require("./config.json");
-// const fs = require("node:fs");
-// const path = require("node:path");
-
-// const commands = [];
-
-// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// client.once(Events.ClientReady, () => {
-//   console.log(`Ready! Logged in as ${client.user.tag}`);
-//   client.user.setActivity("Welcome Gamers");
-// });
-
-// client.commands = new Collection();
-// client.SlashCommands = new Collection();
-
-// const commandsPath = path.join(__dirname, "/commands");
-// const commandFiles = fs
-//   .readdirSync(commandsPath)
-//   .filter((file) => file.endsWith(".js"));
-
-// for (const file of commandFiles) {
-//   const filePath = path.join(commandsPath, file);
-//   const command = require(filePath);
-//   commands.push(command.data.toJSON());
-//   if ("data" in command && "execute" in command) {
-//     client.commands.set(command.data.name, command);
-//   } else {
-//     console.log(
-//       `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-//     );
-//   }
-// }
-
-// const rest = new REST({ version: "10" }).setToken(token);
-// (async () => {
-//   try {
-//     console.log(
-//       `Started refreshing ${commands.length} application (/) commands.`
-//     );
-
-//     const data = await rest.put(
-//       Routes.applicationGuildCommands(clientId, guildId),
-//       { body: commands }
-//     );
-//     console.log(
-//       `Successfully reloaded ${data.length} application (/) commands.`
-//     );
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
-
-// client.on(Events.InteractionCreate, async (interaction) => {
-//   if (!interaction.isChatInputCommand()) return;
-
-//   const command = interaction.client.commands.get(interaction.commandName);
-//   if (!command) {
-//     console.error(`No command matching ${interaction.commandName} was found.`);
-//     return;
-//   }
-//   try {
-//     await command.execute(interaction);
-//   } catch (error) {
-//     console.error(error);
-//     await interaction.reply({
-//       content: "There was an error while executing this command!",
-//       ephemeral: true,
-//     });
-//   }
-// });
-
-// client.login(token);
-
 const {
   Client,
   Collection,
   GatewayIntentBits,
   Partials,
+  ActivityType,
 } = require("discord.js");
 const { loadCommands } = require("./handlers/commandHandler.js");
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
@@ -92,6 +11,16 @@ const { User, Message, GuildMember, ThreadMember } = Partials;
 const client = new Client({
   intents: [Guilds, GuildMembers, GuildMessages],
   partials: [User, Message, GuildMember, ThreadMember],
+  presence: {
+    status: "online",
+    afk: false,
+    activities: [
+      {
+        name: "Welcome Gamers",
+        type: ActivityType.Playing,
+      },
+    ],
+  },
 });
 
 const { loadEvents } = require("./handlers/eventHandler.js");
